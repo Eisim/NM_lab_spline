18763238bf73f48383f5557886c1762f4c393865
Eisim <koctik.w@mail.ru>
2024-03-25 00:52:08 +0300

    commit for not forget

---
+++ UI/mainWindow.py
@@ -0,0 +1,303 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QLabel
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, \
+    NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+from UI.infoWindow import UI_infoWindow
+
+import ctypes
+import os
+import time
+
+from table_columns import columns, extra_info_rows
+
+ui_file = './UI/MainWindow.ui'
+
+
+def create_plot(parent):
+    parent.fig = Figure(figsize=(parent.width() / 100, parent.height() / 100))
+    parent.canvas = FigureCanvas(parent.fig)
+    parent.plot = parent.fig.add_subplot()
+    return parent.plot
+
+
+class UI_mainWindow(QMainWindow):
+    def __init__(self):
+
+        super(UI_mainWindow, self).__init__()
+        uic.loadUi(ui_file, self)
+        # определение значений по умолчанию
+
+        # создание окон для графиков
+        self.plt = create_plot(self.plot_widget_1) # Функция и сплайн на одном графике
+        self.plt_PS = create_plot(self.plot_widget_2) # График первых производных функции и сплайна
+        self.plt_QS = create_plot(self.plot_widget_3) # Погрешности на одном графике
+
+        # присвоение мест для окон
+        self.plot_widget_1.canvas.setParent(self.plot_widget_1)
+        self.plot_widget_2.canvas.setParent(self.plot_widget_2)
+        self.plot_widget_3.canvas.setParent(self.plot_widget_3)
+
+        self.tabWidget.currentChanged.connect(
+            self.toolBar_changing)  # задание функционала. В данной строке: Меняет тулбар при переходе на другую вклвдку
+        self.plot_toolBar = NavigationToolbar(self.plot_widget_1.canvas, self)
+
+        self.addToolBar(self.plot_toolBar)  # создание тулбара
+
+        self.plot_button.clicked.connect(
+            self.plotting)  # задание функционала. В данной строке: построение графика при нажатии на кнопку "Построить"
+        self.delete_plot.clicked.connect(
+            self.clear_plots)  # задание функционала. В данной строке: очистка окон от ВСЕХ графиков (чистит все окна(графики и таблицу))
+
+        # Названия осей
+        self.plot_widget_1.plot.set_xlabel("x")
+        self.plot_widget_1.plot.set_ylabel("Значение")
+
+        self.plot_widget_2.plot.set_xlabel("x")
+        self.plot_widget_2.plot.set_ylabel("Значение")
+
+        self.plot_widget_3.plot.set_xlabel("x")
+        self.plot_widget_3.plot.set_ylabel("Значение")
+
+        # настройка включения второго окна
+        self.info_button.triggered.connect(lambda: self.info_window("my_info.pdf"))
+
+    def info_window(self,file_name):
+        self.i_window = QMainWindow()
+        self.i_window.ui = UI_infoWindow(file_name)
+        self.i_window.ui.show()
+
+    def clear_plots(self):
+        self.plt.cla()
+        self.plt_PS.cla()
+        self.plot_widget_1.canvas.draw()  # обновление окна
+        self.plot_widget_2.canvas.draw()
+        self.plot_widget_3.canvas.draw()
+
+        self.clear_exrta_info_table()
+        self.clear_table(self.info_table)
+        self.clear_table(self.info_table_2)
+        # self.clear_table(self.info_table_V_dot)
+
+        # Названия осей
+        self.plot_widget_1.plot.set_xlabel("x")
+        self.plot_widget_1.plot.set_ylabel("Значение")
+
+        self.plot_widget_2.plot.set_xlabel("x")
+        self.plot_widget_2.plot.set_ylabel("Значение")
+
+        self.plot_widget_3.plot.set_xlabel("x")
+        self.plot_widget_3.plot.set_ylabel("Значение")
+
+    def toolBar_changing(self, index):  # изменение привязки тулбара
+        self.removeToolBar(self.plot_toolBar)
+        if index == 0:  # тулбал для вкладки # Функция и сплайн на одном графике
+            self.plot_toolBar = NavigationToolbar(self.plot_widget_1.canvas, self)
+        elif index == 1:  # тулбар для вкладки # График первых производных функции и сплайна
+            self.plot_toolBar = NavigationToolbar(self.plot_widget_2.canvas, self)
+        elif index == 2: # тулбар для вкладки # Погрешности на одном графике
+            self.plot_toolBar = NavigationToolbar(self.plot_widget_3.canvas, self)
+        self.addToolBar(self.plot_toolBar)
+
+    def file_to_table(self, file_name):  # из str делает list(list(str))
+        if len(file_name.split('.')) == 1:
+            file_name += '.txt'
+        table = []
+        with open(file_name, 'r') as f:
+            for line in f:
+                table.append(line.split(' '))
+        return table
+
+    def clear_exrta_info_table(self):
+        while self.extra_info_layout.count():
+            item = self.extra_info_layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()
+
+    def update_extra_info_table(self, task_index, table):
+        self.clear_exrta_info_table()
+
+        table = table[0]
+        i = 0
+        cur_table = extra_info_rows[task_index]
+        for elem in table:
+            cur_text = f"{cur_table[i]} {elem}"
+            self.extra_info_layout.addWidget(QLabel(cur_text, self))
+            i += 1
+
+    def plotting(self):
+        # lib_dir = os.path.join(os.curdir, "libNM1_lib.dll")
+        lib_dir = os.path.join(os.curdir, 'dll', 'Release', "labSplines_lib.dll") # Что запускаем
+        lib = ctypes.windll.LoadLibrary(lib_dir)
+
+
+
+        # Начальные значения системы (w,s)
+        n = int(self.get_num_uzlov()) # Количество УЧАСТКОВ
+
+        # Задача
+        task = self.get_task() # Тестовая | Основная | Осцилляция
+        # Функция из 3-х для основной задачи (см. видос Капкаева)
+        task_func = self.get_task_main_func() # f1 | f2 | f3
+
+        my_func =lib.f_test
+        # if task == 0:
+        #
+        #     # my_func = lib.run_progonka_test
+        #     file_name_1 = "test_method_1"
+        #     # file_name_for_V_dot = "rigid_syst_data_s"
+        #
+        # else:
+        #     # my_func = lib.run_main_method_2_const_step
+        #     # file_name = "main_method_2_1_const_step_v"
+        #     # file_name_for_V_dot = "main_method_2_1_const_step_v_dot"
+        #     # file_name_extra_info = 'main_method_2_2_const_step'
+        #
+        #     # my_func = lib.run_progonka_main
+        #     file_name = "main_method_1"
+
+        file_name_1 = "table_1"
+        file_name_2 = "table_2"
+
+        file_name_3 = "for_plots"
+
+        file_name_extra_info = 'spravka'
+        # my_func.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_int, ctypes.c_double,
+        #                     ctypes.c_double,
+        #                     ctypes.c_double, ctypes.c_double, ctypes.c_double]
+        my_func.argtypes = [ctypes.c_int,ctypes.c_double,ctypes.c_double,ctypes.c_double,ctypes.c_double]
+        my_func.restype = ctypes.c_void_p
+        my_func(n, 0, 0, 0, 1)
+
+        # self.clear_table(self.info_table_V_dot)
+
+
+        # self.set_table(self.info_table_V_dot, self.file_to_table(file_name_for_V_dot), file_name_for_V_dot)
+
+        self.clear_table(self.info_table)
+        table_1 = self.file_to_table(file_name_1)  # Парсинг файла в табличный вид (тип ячейки:str)
+
+        self.set_table(self.info_table, table_1, file_name_1)  # заполнение таблицы(вкладка "Таблица")
+
+        self.clear_table(self.info_table_2)
+
+        table_2 = self.file_to_table(file_name_2)  # Парсинг файла в табличный вид (тип ячейки:str)
+
+        self.set_table(self.info_table_2, table_2, file_name_2)  # заполнение таблицы(вкладка "Таблица")
+
+        table_extra_info = self.file_to_table(file_name_extra_info)
+        self.update_extra_info_table(file_name_extra_info,table_extra_info)  # заполнение вспомогательной информации(правый нижний угол)
+
+        table_3 = self.file_to_table(file_name_3)  # Парсинг файла в табличный вид (тип ячейки:str) # Здесь графики
+
+        X_arr = [float(row[0]) for row in table_3]
+        F_arr = [float(row[1]) for row in table_3]
+        S_arr = [float(row[2]) for row in table_3]
+        Diff_arr = [float(row[3]) for row in table_3]
+
+        F_dot_arr = [float(row[4]) for row in table_3]
+        S_dot_arr = [float(row[5]) for row in table_3]
+        Diff_dot_arr = [float(row[6]) for row in table_3]
+
+        F_ddot_arr = [float(row[7]) for row in table_3]
+        S_ddot_arr = [float(row[8]) for row in table_3]
+        Diff_ddot_arr = [float(row[9]) for row in table_3]
+
+        self.plt.plot(X_arr,F_arr,label = "Функция")
+        self.plt.plot(X_arr, S_arr, label="Сплайн")
+        self.plt.set_xlim(auto=True)
+        self.plt.set_ylim(auto=True)
+        self.plt.legend(loc="upper left")
+
+        self.plt_PS.plot(X_arr, F_dot_arr, label="Производная функции")
+        self.plt_PS.plot(X_arr, S_dot_arr, label="Производная сплайна")
+        self.plt_PS.set_xlim(auto=True)
+        self.plt_PS.set_ylim(auto=True)
+        self.plt_PS.legend(loc="upper left")
+
+        self.plt_QS.plot(X_arr, Diff_arr, label="Погрешность сплайна")
+        self.plt_QS.plot(X_arr, Diff_dot_arr, label="Погрешность первой производной")
+        self.plt_QS.plot(X_arr, Diff_ddot_arr, label="Погрешность второй производной")
+        self.plt_QS.set_xlim(auto=True)
+        self.plt_QS.set_ylim(auto=True)
+        self.plt_QS.legend(loc="upper left")
+
+        # self.plt_PS.plot(X_arr, Diff_arr, label="Погрешность")
+        # self.plt_PS.legend(loc="upper left")
+        # if task == 0:
+        #     labels = ["Аналитическое решение","Численное решение"]
+        # else:
+        #     labels = ["Численное решение","Численное решение удв. шагом"]
+        # self.plt.plot(X_arr, U_arr, label=labels[0])
+        # # self.plt.scatter(X_start, w_0,label="Старт. точка (V1)")  # scatter - построение точечного графика. В данном случае просто ставит точку (x0,u0)
+        #
+        # self.plt.plot(X_arr, V_arr, label=labels[1])
+        # self.plt.scatter(X_start, s_0,label="Старт. точка (V2)")
+
+
+        # self.plt.legend(loc="upper left")  # legend - задание окна легенд
+
+        self.plot_widget_1.canvas.draw()
+        self.plot_widget_2.canvas.draw()
+        self.plot_widget_3.canvas.draw()
+
+    # def get_X_start(self):
+    #     return self.X_start.text()
+    #
+    # def get_X_end(self):
+    #     return self.X_end.text()
+
+    # def get_U0(self):
+    #     return self.U_X0.text()
+    #
+    # def get_DU0(self):
+    #     return self.DU_X0.text()
+
+    # def get_start_step(self):
+    #     return self.step_start.text()
+    #
+    # def get_step_control(self):
+    #     return self.step_control.text()
+    #
+    # def get_border_control(self):
+    #     return self.border_control.text()
+
+    # def get_task(self):
+    #     return self.task_selection_box.currentIndex(), self.task_selection_box.currentText()
+
+    # def get_step_mode(self):
+    #     return self.step_mode.isChecked()
+
+    def set_row(self, table, row):
+        max_row_index = table.rowCount()
+        table.insertRow(max_row_index)  # создание строки
+        for i in range(len(row)):
+            table.setItem(max_row_index, i, QTableWidgetItem(str(row[i])))  # заполнение элементами
+
+    def set_columns(self, table, task_index):
+        cols = columns[task_index]
+        table.setColumnCount(len(cols))  # создание пустых колонок, в количестве len(cols) штук
+        table.setHorizontalHeaderLabels(cols)  # присвоение имен для колонок
+
+    def set_table(self, table, data, task_index):
+        self.set_columns(table, task_index)
+        for row in data:
+            self.set_row(table, row)
+
+    def clear_table(self, table):
+        while (table.rowCount() > 0):
+            table.removeRow(0)
+
+    # def get_num_max_iter(self):
+    #     return self.max_num_iter.text()
+
+    def get_num_uzlov(self):
+        return self.num_uzlov.text()
+
+    def get_task(self):
+        return self.comboBox.currentIndex()
+
+    def get_task_main_func(self):
+        return self.comboBox_functions.currentIndex()
---
+++ algorithm/Sp (1).cpp
@@ -0,0 +1,360 @@
+﻿// Sp.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include<vector>
+#include <math.h> // добавляем математические функции
+#include <fstream>
+#include<iomanip>
+
+std::vector<double> progon(std::vector<std::vector<double>> matr, std::vector<double> b) {
+	std::vector<double> res;
+	std::vector<double> alfa;
+	std::vector<double> betta;
+	std::vector<double> temp;
+	alfa.push_back((-matr.at(0).at(1)) / matr.at(0).at(0));
+	betta.push_back(b.at(0) / matr.at(0).at(0));
+	int i = 1;
+	double x = 0;
+	while (i < b.size() - 1) {
+		x = matr.at(i).at(1) + matr.at(i).at(0) * alfa.at(i - 1);
+		alfa.push_back(-matr.at(i).at(2) / x);
+		betta.push_back((b.at(i) - matr.at(i).at(0) * betta.at(i - 1)) / x);
+		i++;
+	}
+	x = matr.at(i).at(1) + matr.at(i).at(0) * alfa.at(i - 1);
+	betta.push_back((b.at(i) - matr.at(i).at(0) * betta.at(i - 1)) / x);
+
+	temp.push_back(betta.at(i));
+	i--;
+	while (i > -1) {
+		temp.push_back(alfa.at(i) * temp.back() + betta.at(i));
+		i--;
+	}
+	int j = temp.size();
+	res.resize(j);
+
+	std::vector<double>::iterator Iter = res.begin();
+	while (j != 0) {
+		*Iter = (temp.at(j - 1));
+		j--;
+		Iter++;
+	}
+	return res;
+}
+
+double f1(double x) {
+	return (pow(x, 3) + 3 * pow(x, 2));
+}
+
+double f2(double x) {
+	return (-pow(x, 3) + 3 * pow(x, 2));
+}
+double f12(double x) {
+	return sin(x) / x;
+}
+
+class Spline {
+
+
+private:
+
+	double S(double x, int i) { // x -аргумент i - номер S1 S2 S3 и тд // канон вид S= ai + bi(x-xi) + ci/2 (x-xi)^2 + di/6(x-xi)^3
+		return A[i - 1] + B[i - 1] * (x - X_i[i]) + C[i] / 2 * pow((x - X_i[i]), 2) + D[i - 1] / 6 * pow((x - X_i[i]), 3);
+
+	}
+	double derivative_1(double f(double), double x, double h) { // первая производная f' = (f(x+h) - f(x-h))/2h
+		return (f(x + h) - f(x - h)) / (2 * h);
+	}
+	double derivative_1(double x, int i) { // первая производная для сплайна bi*x + ci *(x-xi) + di/2(x-xi)^2
+		return +B[i - 1] + C[i] * (x - X_i[i]) + D[i - 1] / 2 * pow((x - X_i[i]), 2);
+	}
+	double derivative_2(double f(double), double x, double h) { // вторая производная f'' = (f(x+h) -2f(x) + f(x-h))/h^2
+		return (f(x + h) - 2 * f(x) + f(x - h)) / pow(h, 2);
+	}
+	double derivative_2(double x, int i) { // вторая производная для сплайна ci *x + di(x - xi)
+		return C[i] + D[i - 1] * (x - X_i[i]);
+	}
+
+	void find_C(double f(double), double g(double)) { //  find_P : P ={A,B,C,D} поиск соответственных коэффециэнтов , формулы можно в методичке по сплайнам найти (Для условий S''(a) =M1 , S''(b) =M2 !!!)
+		if (n == 2) {
+			C.push_back(M1);
+			C.push_back((6 / h * ((F_i[2] - F_i[1]) - (F_i[1] - F_i[0])) - (M1 * h) - (M2 * h)) / (4 * h));
+			C.push_back(M2);
+			find_A();
+			find_B();
+			find_D();
+			if (f == g) {
+				examination(f);
+			}
+			else {
+				examination(f, g);
+			}
+		}
+		else {
+			int m = n - 1;
+			std::vector<std::vector <double>> matr; // n-1 x n-1
+			std::vector<double> str; // левая часть , строка n - 1
+			std::vector<double> rhs; // правая часть n - 1
+			rhs.push_back(6 / h * ((F_i[2] - F_i[1]) - (F_i[1] - F_i[0])) - (M1 * h)); // первая строка матрицы
+			str.push_back(4 * h);
+			str.push_back(h);
+			matr.push_back(str);
+			/*
+			если шаг константа то матрица будет иметь след вид:
+			4h h 0 ... 0         |то что там с ф - M1 * h
+			h 4h h ... 0		 |
+			0 h 4h h ... 0		 | то что там с Ф
+			...               =  |
+			...					 |
+			0 ... h 4h           | то что там с Ф - M2 * h
+			для прогонки мне надо передать не нулевые коэффециэнты
+			*/
+
+			for (int i = 2; i <= m - 1; i++) { // подсчет строк со 2 по m-2
+				str.clear();
+				str.push_back(h);
+				str.push_back(4 * h);
+				str.push_back(h);
+				matr.push_back(str);
+				rhs.push_back(6 / h * ((F_i[i + 1] - F_i[i]) - (F_i[i] - F_i[i - 1])));
+			}
+			str.clear();
+			rhs.push_back(6 / h * ((F_i[m + 1] - F_i[m]) - (F_i[m] - F_i[m - 1])) - (M2 * h)); // подсчет последний строки
+			str.push_back(h);
+			str.push_back(4 * h);
+			matr.push_back(str);
+			C = progon(matr, rhs);
+			auto iter = C.cbegin();
+			C.emplace(iter, M1);
+			C.push_back(M2);
+			find_A();
+			find_B();
+			find_D();
+			if (f == g) {
+				examination(f);
+			}
+			else {
+				examination(f, g);
+			}
+		}
+	}
+
+	void find_A() {
+		for (int i = 1; i <= n; i++) A.push_back(F_i[i]);
+	}
+	void find_D() {
+		for (int i = 1; i <= n; i++) D.push_back((C[i] - C[i - 1]) / h);
+	}
+	void find_B() {
+		for (int i = 1; i <= n; i++) {
+			B.push_back((F_i[i] - F_i[i - 1]) / h + C[i] * h / 3 + C[i - 1] * h / 6);
+		}
+	}
+	void clear() {
+		C.clear();
+		A.clear();
+		B.clear();
+		D.clear();
+		X_i.clear();
+		X_2_i.clear();
+		F_i.clear();
+		r_1.clear();
+		r_2.clear();
+		r_3.clear();
+
+	}
+	void examination(double f(double)) { // удваивает начальное n пока не добьеться нужной точности   ///в производной шаг константный у меня , но если надо можете туда что то другое передавать
+		double x = 0;
+		double X_max;
+		int j = 1;
+		double max_1 = fabs(f(a) - S(a, 1));
+		double max_2 = fabs(derivative_1(f, a, 0.001) - derivative_1(a, 1));
+		double max_3 = fabs(derivative_2(f, a, 0.001) - derivative_2(a, 1));
+		for (int i = 0; i <= 2 * n; i++) {
+			x = a + h / 2 * i;
+			X_2_i.push_back(x);
+			if (x <= X_i[j]) {
+				if (fabs(f(x) - S(x, j)) > max_1) {
+					max_1 = fabs(f(x) - S(x, j));
+					X_max = x;
+				}
+				if ((fabs(derivative_1(f, x, 0.001) - derivative_1(x, j))) > max_2) max_2 = fabs(derivative_1(f, x, 0.001) - derivative_1(x, j));
+				if ((fabs(derivative_2(f, x, 0.001) - derivative_2(x, j))) > max_3) max_3 = fabs(derivative_2(f, x, 0.001) - derivative_2(x, j));
+			}
+
+			else {
+				j++;
+				if (fabs(f(x) - S(x, j)) > max_1) {
+					max_1 = fabs(f(x) - S(x, j));
+					X_max = x;
+				}
+				if ((fabs(derivative_1(f, x, 0.001) - derivative_1(x, j))) > max_2) max_2 = fabs(derivative_1(f, x, 0.001) - derivative_1(x, j));
+				if ((fabs(derivative_2(f, x, 0.001) - derivative_2(x, j))) > max_3) max_3 = fabs(derivative_2(f, x, 0.001) - derivative_2(x, j));
+			}
+			r_1.push_back(fabs(f(x) - S(x, j)));
+			r_2.push_back(fabs(derivative_1(f, x, 0.001) - derivative_1(x, j)));
+			r_3.push_back(fabs(derivative_2(f, x, 0.001) - derivative_2(x, j)));
+
+		}
+
+
+		max_eps = max_1;
+		if ((max_eps - 1e-06) > 0) {
+			this->n = n * 2;
+			clear();
+			this->h = abs(a - b) / n;
+			A_12(f12);
+		}
+		else {
+			std::cout << " n =  " << n << std::endl; // это все теперь в файл фигануть надо, но предлагаю сначало консольно сделать.
+			std::cout << " max1(F(x) - S(x)) =  " << max_1 << std::endl;
+			std::cout << " max2(F'(x) - S'(x)) =  " << max_2 << std::endl;
+			std::cout << " max3(F''(x) - S''(x)) =  " << max_3 << std::endl;
+		}
+	}
+
+	void examination(double f(double), double g(double)) { // проверяет корректность тестовой задачи (A)
+		double x = 0;
+		int j = 1;
+		double max_1 = fabs(f(a) - S(a, 1));
+		double max_2 = fabs(derivative_1(f, a, 0.01) - derivative_1(a, 1));
+		double max_3 = fabs(derivative_2(f, a, 0.01) - derivative_2(a, 1));
+		for (int i = 0; i <= 2 * n; i++) {
+			x = -1 + h / 2 * i;
+			X_2_i.push_back(x);
+			if (x <= X_i[j]) {
+				if (x <= 0) {
+					if (fabs(f(x) - S(x, j)) > max_1) max_1 = fabs(f(x) - S(x, j));
+					if (fabs(derivative_1(f, x, 0.01) - derivative_1(x, j))) max_2 = fabs(derivative_1(f, x, 0.01) - derivative_1(x, j));
+					if (fabs(derivative_2(f, x, 0.01) - derivative_2(x, j))) max_3 = fabs(derivative_2(f, x, 0.01) - derivative_2(x, j));
+					r_1.push_back(fabs(f(x) - S(x, j)));
+					r_2.push_back(fabs(derivative_1(f, x, 0.001) - derivative_1(x, j)));
+					r_3.push_back(fabs(derivative_2(f, x, 0.001) - derivative_2(x, j)));
+				}
+				else {
+					if (fabs(g(x) - S(x, j)) > max_1) max_1 = fabs(g(x) - S(x, j));
+					if (fabs(derivative_1(g, x, 0.01) - derivative_1(x, j))) max_2 = fabs(derivative_1(g, x, 0.01) - derivative_1(x, j));
+					if (fabs(derivative_2(g, x, 0.01) - derivative_2(x, j))) max_3 = fabs(derivative_2(g, x, 0.01) - derivative_2(x, j));
+					r_1.push_back(fabs(g(x) - S(x, j)));
+					r_2.push_back(fabs(derivative_1(g, x, 0.001) - derivative_1(x, j)));
+					r_3.push_back(fabs(derivative_2(g, x, 0.001) - derivative_2(x, j)));
+				}
+			}
+			else {
+				j++;
+				if (x <= 0) {
+					if (fabs(f(x) - S(x, j)) > max_1) max_1 = fabs(f(x) - S(x, j));
+					if (fabs(derivative_1(f, x, 0.01) - derivative_1(x, j))) max_2 = fabs(derivative_1(f, x, 0.01) - derivative_1(x, j));
+					if (fabs(derivative_2(f, x, 0.01) - derivative_2(x, j))) max_3 = fabs(derivative_2(f, x, 0.01) - derivative_2(x, j));
+					r_1.push_back(fabs(f(x) - S(x, j)));
+					r_2.push_back(fabs(derivative_1(f, x, 0.001) - derivative_1(x, j)));
+					r_3.push_back(fabs(derivative_2(f, x, 0.001) - derivative_2(x, j)));
+				}
+				else {
+					if (fabs(g(x) - S(x, j)) > max_1) max_1 = fabs(g(x) - S(x, j));
+					if (fabs(derivative_1(g, x, 0.01) - derivative_1(x, j))) max_2 = fabs(derivative_1(g, x, 0.01) - derivative_1(x, j));
+					if (fabs(derivative_2(g, x, 0.01) - derivative_2(x, j))) max_3 = fabs(derivative_2(g, x, 0.01) - derivative_2(x, j));
+					r_1.push_back(fabs(g(x) - S(x, j)));
+					r_2.push_back(fabs(derivative_1(g, x, 0.001) - derivative_1(x, j)));
+					r_3.push_back(fabs(derivative_2(g, x, 0.001) - derivative_2(x, j)));
+				}
+			}
+		}
+		// это все теперь в файл фигануть надо, но предлагаю сначало консольно сделать.
+		std::cout << " max1(F(x) - S(x)) =  " << max_1 << std::endl;
+		std::cout << " max2(F'(x) - S'(x)) =  " << max_2 << std::endl;
+		std::cout << " max3(F''(x) - S''(x)) =  " << max_3 << std::endl;
+
+	}
+	void test_func(double f(double), double g(double)) { // пункт а) тестовая задача.
+		double x = 0;
+		for (int i = 0; i <= n; i++) {
+			x = -1 + h * i;
+			X_i.push_back(x);
+			if (x <= 0) {
+				F_i.push_back(f(x));
+			}
+			else {
+				F_i.push_back(g(x));
+			}
+		}
+		//for (int i = 0; i <= n; i++) {
+			//std::cout << "X_i : " << X_i[i] << " F_i : " << F_i[i] << std::endl;
+		//}
+		find_C(f, g);
+
+		return;
+	}
+	void A_12(double f(double)) {
+		double x = 0;
+		for (int i = 0; i <= n; i++) {
+			x = a + h * i;
+			X_i.push_back(x);
+			F_i.push_back(f(x));
+
+		}
+		find_C(f, f);
+	}
+public:
+	// это все теперь в файл фигануть надо, но предлагаю сначало консольно сделать.
+	std::vector<double> X_i; // узлы с шагом h
+	std::vector<double> F_i; // значения функции в узлах 
+	std::vector<double> C; // коэф-ты С
+	std::vector<double> A;// коэф-ты А
+	std::vector<double> B; // коэф-ты В
+	std::vector<double> D; // коэф-ты D
+	std::vector<double> X_2_i; // узлы с шагом h/2
+	std::vector<double> r_1; // F - S
+	std::vector<double> r_2; // F' - S'
+	std::vector<double> r_3; // F'' - S''
+	double M1 = 0;// S''(a)
+	double M2 = 0;// S''(b)
+	double h, a, b; // Шаг, левая граница, правая граница
+	int n; // число разбиений  -> число узлов n+1
+	double max_eps; // точность 
+
+	Spline(int n, double M1, double M2, double a, double b) { // проверку на корректность n  не делаю, это в интерфейсе должно быть.
+		this->n = n;
+		this->M1 = M1;
+		this->M2 = M2;
+		this->a = a;
+		this->b = b;
+		this->h = abs(a - b) / n;
+	}
+	void Run(int value) {
+		switch (value)
+		{
+		case 1:
+			test_func(f1, f2);
+			break;
+		case 2:
+			A_12(f12);
+			break;
+		default:
+			break;
+		}
+	}
+};
+
+extern "C" __declspec(dllexport) void f_test(int n, double m1, double m2, double a, double b) {
+	Spline sp(n, m1, m2, a, b);
+	sp.Run(1);
+	std::ofstream table_1("data/table_1.txt");
+	std::ofstream table_2("data/table_2.txt");
+	std::ofstream spravka("data/spravka.txt");
+	std::ofstream for_plots("data/for_plots.txt");
+	for (int i = 1; i < sp.n; i++) {
+		table_1 << (i) << ' ' << (sp.X_i[i - 1]) << ' ' << (sp.X_i[i]) << ' ' << sp.A[i] << ' ' << sp.B[i] << ' ' << sp.C[i] << ' ' << sp.D[i] << '\n';
+	}
+	table_1.close();
+}
+
+
+
+
+int main()
+{
+	f_test(10, 0, 0, 0, 1);
+}
+
+
